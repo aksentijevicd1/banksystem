c898d4971ed31bb9ed0eb25b03c8b4a453f174de
aksentijevicd1 <dusanaksentijevic10@gmail.com>
2024-03-09 14:07:43 +0100

    Test, before handling deadlock

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,14 @@
+package db
+
+/*
+func createRandomEntry(t *testing.T, account Account) Entry {
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+*/
